ce9f5e87dcf81df74d3beb0c0650a5e12d1c6da0
Andro1907 <121785451+Andro1907@users.noreply.github.com>
2023-01-03 09:25:30 +0530

    Add files via upload

---
+++ Digital/Digital.js
@@ -0,0 +1,26 @@
+const hr=document.getElementById("hour");
+const min=document.getElementById("minutes");
+const sec=document.getElementById("seconds");
+const time=document.getElementById("ampm");
+
+function updateClock(){
+    let h=new Date().getHours();
+    let m=new Date().getMinutes();
+    let s=new Date().getSeconds();
+    let ampm="AM";
+
+    if(h>12){
+        h=h-12;
+        ampm="PM";
+    }
+
+    h=h<10 ? "0"+h:h;
+    m=m<10 ? "0"+m:m;
+    s=s<10 ? "0"+s:s;
+
+    hr.innerText=h;
+    min.innerText=m;
+    sec.innerText=s;
+    time.innerText=ampm;
+}
+setInterval(updateClock,1000);
